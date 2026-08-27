46a0d4ddc421ae477c2f68d1d77da066f3e8d480
Aadith M Mathew <aadithmathew2017@gmail.com>
2026-08-27 23:46:48 +0530

    M0(scenes): create main launcher scene and input smoke script (#2)

---
+++ scenes/main.gd
@@ -0,0 +1,16 @@
+class_name MainLauncher
+extends Control
+
+@onready var title_label: Label = $CenterContainer/TitleLabel
+
+func _ready() -> void:
+	var vp_size: Vector2 = get_viewport_rect().size
+	print("[MainLauncher] Viewport initialized: ", vp_size)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch:
+		var touch: InputEventScreenTouch = event as InputEventScreenTouch
+		print("[MainLauncher] Touch event: index=", touch.index, " pos=", touch.position, " pressed=", touch.pressed)
+	elif event is InputEventScreenDrag:
+		var drag: InputEventScreenDrag = event as InputEventScreenDrag
+		print("[MainLauncher] Drag event: index=", drag.index, " pos=", drag.position, " relative=", drag.relative)
