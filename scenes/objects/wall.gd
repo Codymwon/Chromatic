9ac22c1e95a38ef9feb21ff05956815b4159e72e
Aadith M Mathew <aadithmathew2017@gmail.com>
2026-09-11 10:48:07 +0530

    M2(objects): streamline wall component and remove unrequested size export (#1)

--- scenes/objects/wall.gd
+++ scenes/objects/wall.gd
@@ -1,25 +1,11 @@
 class_name Wall
 extends StaticBody2D
 
-@export var collider_type: BeamTypes.ColliderType = BeamTypes.ColliderType.WALL
-@export var size: Vector2 = Vector2(40.0, 300.0):
-	set(value):
-		size = value
-		_update_size()
+const BeamTypes = preload("res://core/beam_types.gd")
 
-@onready var collision_shape: CollisionShape2D = get_node_or_null("CollisionShape2D") as CollisionShape2D
-@onready var visual: ColorRect = get_node_or_null("Visual") as ColorRect
+@export var collider_type: BeamTypes.ColliderType = BeamTypes.ColliderType.WALL
 
 func _ready() -> void:
 	collision_layer = 1
 	collision_mask = 0
-	_update_size()
 
-func _update_size() -> void:
-	if collision_shape != null and collision_shape.shape is RectangleShape2D:
-		(collision_shape.shape as RectangleShape2D).size = size
-	if visual != null:
-		visual.offset_left = -size.x / 2.0
-		visual.offset_top = -size.y / 2.0
-		visual.offset_right = size.x / 2.0
-		visual.offset_bottom = size.y / 2.0
